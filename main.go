fff3a7572eafc6576677d558c26bc33b1f539e89
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2018-02-15 16:04:39 +0700

    add handler

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func Add(num1 int, num2 int) int {
 	return num1 + num2
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-	result := Add(1, 2)
-	fmt.Println(result)
+	fmt.Println("Apps started....")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8181", nil)
 }

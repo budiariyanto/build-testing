7c0abfe9a862036cb0e4443c2ecc61170a55705e
Budi Ariyanto <fx.budi.ariyanto@gmail.com>
2018-02-16 12:41:37 +0700

    testing 123

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
+
+var stdOutLog = log.New(os.Stdout, "Access: ", log.Llongfile)
+var stdErrLog = log.New(os.Stderr, "Error: ", log.Llongfile)
 
 func Add(num1 int, num2 int) int {
 	return num1 + num2
 }
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	stdOutLog.Println("Ini access log")
+	stdErrLog.Println("Ini error log")
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	handler(w, r)
 }
 
 func main() {
